739c62c7c1d72c73bda3a9bff8f8885b1d527a5f
Tyler Crill <Tyler.W.Crill@hsutx.edu>
2018-06-17 15:01:01 -0500

    Cleaned up code in IbeforeE

--- Daily Programmer Challenges/#363 - I before E/IbeforeE.java
+++ Daily Programmer Challenges/#363 - I before E/IbeforeE.java
@@ -1,30 +1,29 @@
+/**
+ * https://www.reddit.com/r/dailyprogrammer/comments/8q96da/20180611_challenge_363_easy_i_before_e_except/
+ */
+
 import java.io.*;
 public class IbeforeE {
     private static int exceptions, numTrue, numFalse;
 
     public static void main(String... args) throws IOException {
-
-        System.out.println(checkExpanded("a"));
-        System.out.println(checkExpanded("zombie"));
-        System.out.println(checkExpanded("transceiver"));
-        System.out.println(checkExpanded("veil"));
-        System.out.println(checkExpanded("icier"));
-        System.out.println("********************\n\n");
-        System.out.println(checkExpanded("ciecei"));
-        System.out.println(checkExpanded("ceiei"));
-
-        System.out.println("Checking for exceptions in enable1.txt");
-
-        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\asdf\\Documents\\Programming\\Java Work\\Daily Programmer Challenges\\#363 - I before E\\enable1.txt"));
-
+        String[] arr = {"a", "zombie", "transceiver", "veil", "icier", "ciecei", "ceiei"};
+        for(String str : arr){
+            System.out.println(check(str));
+        }
+        // Code for bonus area below
         exceptions = 0;
         numTrue = 0;
         numFalse = 0;
 
+        System.out.println("\n\nChecking for exceptions in enable1.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\asdf\\Documents\\Programming\\Java Work\\Daily Programmer Challenges\\#363 - I before E\\enable1.txt"));
+
         String line;
         while ((line = br.readLine()) != null) {
-            checkExpanded(line);
-            //System.out.println(line);
+            check(line);
+
         }
 
 
@@ -34,22 +33,15 @@ public class IbeforeE {
 
     }
 
-    private static boolean check(String str) {
-        if ((str.contains("ei") || str.contains("cie")) && !str.contains("cei")) {
-            exceptions++;
-            numFalse++;
-            return false;
-        }
-        numTrue++;
-        return true;
-    }
-    private static boolean checkExpanded(String str){
+   private static boolean check(String str){
 
         if(!str.contains("ei")){
            if(str.contains("cie")){
                exceptions++;
+               numFalse++;
                return false;
            }
+           numTrue++;
            return true;
         }
         if(str.contains("cei") && str.length() < 5){
